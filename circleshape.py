08c53eb9a79c9623ee6bf341808d7e0211584be3
jos705 <jos.snijder@gmail.com>
2025-05-07 21:22:30 +0200

    exit game when hit asteroid

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,8 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collisioncheck(self, otherShape):
+        distance = pygame.math.Vector2.distance_to(self.position, otherShape.position)
+        if distance <= (self.radius + otherShape.radius):
+            return True
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
 from circleshape import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 
 
@@ -18,14 +21,22 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Asteroid.containers = (asteroids, updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroidfield = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         for updates in updatable:
             updates.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collisioncheck(player):
+                print("Game Over!")
+                sys.exit()
         screen.fill((0 ,0 ,0 ))
         for obj in drawable:
             obj.draw(screen)
